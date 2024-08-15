d470105fa23f01120be535a8f7af9a2be3da454f
Ali Mansour <alialhadimansour21@gmail.com>
2024-08-15 17:00:36 +0300

    Create the users migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->date('date_of_birth');
+            $table->enum('gender', ['male', 'female']);
+            $table->enum('role', ['member', 'guide', 'admin'])->default('member');
             $table->timestamps();
         });
 
