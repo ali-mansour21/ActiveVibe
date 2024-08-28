b362449125988df64f3f872d0c2fbb3ba3217fe3
Ali Mansour <alialhadimansour21@gmail.com>
2024-08-28 22:15:33 +0300

    Fix the style

--- frontend/src/pages/Home/components/LandingPage .jsx
+++ frontend/src/pages/Home/components/LandingPage .jsx
@@ -1,10 +1,4 @@
 import "../styles/landing.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCaretDown,
-  faLocationDot,
-  faMagnifyingGlass,
-} from "@fortawesome/free-solid-svg-icons";
 const LandingPage = () => {
   return (
     <div className="banner">
@@ -13,20 +7,6 @@ const LandingPage = () => {
         Explore the <span className="highlight">vibrant events</span> happening
         locally and globally.
       </p>
-      <div className="search-bar">
-        <div className="search-input-container">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
-          <input
-            type="text"
-            placeholder="Search Events, Categories, Location..."
-            className="search-input"
-          />
-        </div>
-        <button className="location-button">
-          <FontAwesomeIcon icon={faLocationDot} className="location-icon" />{" "}
-          Mumbai <FontAwesomeIcon icon={faCaretDown} className="list-icon" />
-        </button>
-      </div>
     </div>
   );
 };
