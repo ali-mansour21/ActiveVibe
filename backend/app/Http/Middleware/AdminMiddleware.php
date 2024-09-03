b6f406a35a13fff191ec1d2d5ef288e187e40c6f
Ali Mansour <alialhadimansour21@gmail.com>
2024-09-03 20:05:59 +0300

    Create a middleware for the admin

--- backend/app/Http/Middleware/AdminMiddleware.php
+++ backend/app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -13,8 +14,12 @@ class AdminMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            return $next($request);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'Unauthorized'], 403);
     }
 }
