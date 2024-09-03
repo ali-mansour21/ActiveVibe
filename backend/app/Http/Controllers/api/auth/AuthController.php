6714083eb9d143a491f56188f378a555fdc102b4
Ali Mansour <alialhadimansour21@gmail.com>
2024-09-03 20:02:11 +0300

    Implement the login function

--- backend/app/Http/Controllers/api/auth/AuthController.php
+++ backend/app/Http/Controllers/api/auth/AuthController.php
@@ -53,4 +53,38 @@ class AuthController extends Controller
             ]
         ], 201);
     }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
 }
