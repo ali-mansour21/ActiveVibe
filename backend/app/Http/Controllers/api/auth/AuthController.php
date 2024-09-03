00b42bfabbf8449ef22f3e11ff755e266ae475f4
Ali Mansour <alialhadimansour21@gmail.com>
2024-09-03 19:57:51 +0300

    Create the register function

---
+++ backend/app/Http/Controllers/api/auth/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\api\auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Member;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data =  $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8',
+            'date_of_birth' => 'required|date|before:today',
+            'gender' => 'required|in:male,female',
+            'phone_number' => 'required|string|max:15|regex:/^[0-9\-\(\)\/\+\s]*$/',
+            'emergency_number' => 'required|string|max:15|regex:/^[0-9\-\(\)\/\+\s]*$/',
+            'nationality' => 'required|string|max:100',
+        ]);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' =>  bcrypt($data['password']),
+            'date_of_birth' => $data['date_of_birth'],
+            'gender' => $data['gender'],
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        $formattedDate = Carbon::now()->format('ymd');
+        Member::create([
+            'user_id' => $user->id,
+            'joining_date' => $formattedDate,
+            'phone_number' => $data['phone_number'],
+            'emergency_number' => $data['emergency_number'],
+            'nationality' => $data['nationality'],
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        $token = Auth::login($user);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ], 201);
+    }
+}
